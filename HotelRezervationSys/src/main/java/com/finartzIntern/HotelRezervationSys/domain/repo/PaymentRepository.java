aa5ec68a6f8b97fffdc54c12d7053763779c736a
irembektsa <irem.bkts54@gmail.com>
2026-07-10 12:15:51 +0300

    AuditLogRepository ve PaymentRepository eklendi

---
+++ HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/repo/PaymentRepository.java
@@ -0,0 +1,14 @@
+package com.finartzIntern.HotelRezervationSys.domain.repo;
+
+import com.finartzIntern.HotelRezervationSys.domain.model.entities.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByBookingId(Long bookingId);
+
+}
