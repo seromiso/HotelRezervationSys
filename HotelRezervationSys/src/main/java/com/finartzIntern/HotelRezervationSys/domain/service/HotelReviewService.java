4db529fa25a31c9f8cbe172d17cc155e9e661a90
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2026-07-13 14:13:19 +0200

    review,feature dtolari

---
+++ HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/service/HotelReviewService.java
@@ -0,0 +1,9 @@
+package com.finartzIntern.HotelRezervationSys.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HotelReviewService {
+
+    private final
+}
