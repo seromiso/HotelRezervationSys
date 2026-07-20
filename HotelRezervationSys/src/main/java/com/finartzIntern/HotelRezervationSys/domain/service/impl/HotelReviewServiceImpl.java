282b0de17adb981ed6ac2b3ddc2fdf6a458237f8
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2026-07-20 10:54:06 +0200

    review controller son hali ve exception eklemeleri

--- HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/service/impl/HotelReviewServiceImpl.java
+++ HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/service/impl/HotelReviewServiceImpl.java
@@ -1,31 +1,114 @@
 package com.finartzIntern.HotelRezervationSys.domain.service.impl;
 
+import com.finartzIntern.HotelRezervationSys.domain.exceptions.ConflictException;
+import com.finartzIntern.HotelRezervationSys.domain.exceptions.ResourceNotFoundException;
+import com.finartzIntern.HotelRezervationSys.domain.model.dtos.request.CreateHotelReviewRequestDto;
+import com.finartzIntern.HotelRezervationSys.domain.model.dtos.request.UpdateHotelReviewRequestDto;
 import com.finartzIntern.HotelRezervationSys.domain.model.dtos.response.HotelReviewResponseDto;
+import com.finartzIntern.HotelRezervationSys.domain.model.entities.HotelReviews;
+import com.finartzIntern.HotelRezervationSys.domain.repository.HotelRepository;
 import com.finartzIntern.HotelRezervationSys.domain.repository.HotelReviewRepository;
+import com.finartzIntern.HotelRezervationSys.domain.model.entities.Hotel;
+import com.finartzIntern.HotelRezervationSys.domain.model.entities.User;
+import com.finartzIntern.HotelRezervationSys.domain.repository.UserRepository;
 import com.finartzIntern.HotelRezervationSys.domain.service.HotelReviewService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HotelReviewServiceImpl implements HotelReviewService {
 
     private final HotelReviewRepository hotelReviewRepository;
-    public HotelReviewServiceImpl(HotelReviewRepository hotelReviewRepository){
+    private final HotelRepository hotelRepository;
+    private final UserRepository userRepository;
+
+    public HotelReviewServiceImpl(HotelReviewRepository hotelReviewRepository, HotelRepository hotelRepository, UserRepository userRepository){
         this.hotelReviewRepository = hotelReviewRepository;
+        this.hotelRepository = hotelRepository;
+        this.userRepository = userRepository;
     }
     @Override
     public List<HotelReviewResponseDto> getReviewsByHotelId(Long hotelId) {
-        return hotelReviewRepository.findByHotelId(hotelId).stream().map(HotelReviewResponseDto::from).toList();
+        return hotelReviewRepository.findByHotel_IdOrderByCreatedAtDesc(hotelId).stream().map(HotelReviewResponseDto::from).toList();
+    }
+
+    @Override
+    public HotelReviewResponseDto getReviewById(Long reviewId) {
+        HotelReviews review = hotelReviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Review not found with id: " + reviewId
+                ));
+        return HotelReviewResponseDto.from(review) ;
     }
 
     @Override
     public HotelReviewResponseDto getReviewByReservationId(Long reservationId) {
-        return hotelReviewRepository.findByReservationId(reservationId).map(HotelReviewResponseDto::from).orElseThrow(() -> new RuntimeException("Review not found for reservation id: " + reservationId));
+        return hotelReviewRepository.findByReservationId(reservationId).map(HotelReviewResponseDto::from).orElseThrow(() -> new ResourceNotFoundException("Review not found for reservation id: " + reservationId));
     }
 
     @Override
     public boolean hasReviewForReservation(Long reservationId) {
         return hotelReviewRepository.existsByReservationId(reservationId);
+    }
+
+    @Override
+    public Double getAverageRatingByHotelId(Long hotelId) {
+        Double averageRating = hotelReviewRepository.findAverageRatingByHotelId(hotelId);
+        return averageRating == null ? 0.0 : averageRating;
+    }
+
+    @Override
+    @Transactional
+    public HotelReviewResponseDto createReview(Long hotelId, Long userId, Long reservationId, CreateHotelReviewRequestDto request) {
+        if(hotelReviewRepository.existsByReservationId(reservationId)){
+
+            throw new ConflictException("This reservation already has a review. ");
+        }
+        Hotel hotel = hotelRepository.findById(hotelId)
+                .orElseThrow(() -> new ResourceNotFoundException("Hotel not found with id: " + hotelId));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "User not found with id: " + userId
+                ));
+        HotelReviews review = new HotelReviews();
+        review.setHotel(hotel);
+        review.setUser(user);
+        review.setReservationId(reservationId);
+        review.setRating(request.rating());
+        review.setComment(request.comment());
+
+        HotelReviews savedReview = hotelReviewRepository.save(review);
+
+        return HotelReviewResponseDto.from(savedReview);
+    }
+
+    @Override
+    @Transactional
+    public HotelReviewResponseDto updateReview(Long reviewId, UpdateHotelReviewRequestDto request) {
+        HotelReviews review = hotelReviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Review not found with id: " + reviewId
+                ));
+
+        review.setRating(request.rating());
+        review.setComment(request.comment());
+
+        HotelReviews updatedReview = hotelReviewRepository.save(review);
+
+        return HotelReviewResponseDto.from(updatedReview);
+    }
+
+    @Override
+    @Transactional
+    public void deleteReview(Long reviewId) {
+        HotelReviews review = hotelReviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Review not found with id: " + reviewId
+                ));
+
+        hotelReviewRepository.delete(review);
     }
 }
