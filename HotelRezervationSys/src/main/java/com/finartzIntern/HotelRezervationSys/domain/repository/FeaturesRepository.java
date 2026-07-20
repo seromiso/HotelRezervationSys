b08952fde08ace95e0e278e6cf30a6d354ab4681
onur366 <onurh1279@gmail.com>
2026-07-20 09:58:40 +0300

    FeaturesController ve Feature Service eklendi

--- HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/repository/FeaturesRepository.java
+++ HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/repository/FeaturesRepository.java
@@ -1,9 +1,12 @@
 package com.finartzIntern.HotelRezervationSys.domain.repository;
 
+import com.finartzIntern.HotelRezervationSys.domain.model.entities.Booking;
 import com.finartzIntern.HotelRezervationSys.domain.model.entities.Features;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FeaturesRepository extends JpaRepository<Features,Long> {
+    Optional<Features> findById(Long id);
 }
