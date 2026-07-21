ca4ee15b9d03f39b5bb991c0d0555c15b8ef4d76
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2026-07-21 09:43:18 +0200

    roomtypefeature service interface kismi

--- HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/repository/RoomTypeFeatureRepository.java
+++ HotelRezervationSys/src/main/java/com/finartzIntern/HotelRezervationSys/domain/repository/RoomTypeFeatureRepository.java
@@ -3,5 +3,13 @@ package com.finartzIntern.HotelRezervationSys.domain.repository;
 import com.finartzIntern.HotelRezervationSys.domain.model.entities.RoomTypeFeature;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RoomTypeFeatureRepository extends JpaRepository<RoomTypeFeature,Long> {
+
+    List<RoomTypeFeature> findByRoomType_Id(Long roomTypeId);
+    Optional<RoomTypeFeature> findByRoomType_IdAndFeature_Id(Long roomTypeId, Long featureId);
+    boolean existsByRoomType_IdAndFeature_Id(Long roomTypeId, Long featureId);
+    void deleteByRoomType_IdAndFeature_Id(Long roomTypeId, Long featureId);
 }
